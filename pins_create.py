f0866bfc0d613c0313d185ac8656434eb3733869
SupsKods <supriya.bobba@gmail.com>
2018-12-17 21:17:17 -0500

    Add files via upload

--- pins_create.py
+++ pins_create.py
@@ -2,14 +2,15 @@ import csv
 
 
 #def create_pin_file(in_pin_csv, in_all_csv)
-def create_pin_file():
-    with open('pids-file.csv',mode='rb') as f:
+def create_pin_file(full_data_file,pid_file):
+    with open(pid_file,mode='r') as f:
         f_r = csv.reader(f, delimiter=',')
         rows = [row for row in f_r]
         flat_list = [item for sublist in rows for item in sublist]
 
-    with open('full-orig-data.csv',mode='rb') as f:
-        with open('out-pids.csv', mode = 'wb') as zf:
+    with open(full_data_file,mode='r') as f:
+        out_file = full_data_file.split('.')[0]+'-out.csv'
+        with open(out_file, mode = 'w') as zf:
             f_r = csv.reader(f, delimiter=',')
             zf_w = csv.writer(zf, delimiter=',')
             header = next(f_r)
@@ -25,24 +26,14 @@ def create_pin_file():
                 #print '***************'
 
         
-create_pin_file()
+
  
-##with open('full-data.csv',mode='rb') as f:
-##        with open('test-pins_2.csv', mode = 'wb') as zf:
-##            f_r = csv.reader(f, delimiter=',')
-##            zf_w = csv.writer(zf, delimiter=',')
-##            header = next(f_r)
-##            zf_w.writerow(header)
-##            for row in f_r:
-##                print row
-##                for item in flat_list:
-##			print item
-##			if item in row:
-##				print 'Found matching ' + item
-##                                zf_w.writerow(row)
-##                print '***************'
-##                
-## with open('pins-1.csv',mode='rb') as f:
-##	f_r = csv.reader(f, delimiter=',')
-##	rows = [row for row in f_r]
-##	flat_list = [item for s:Qublist in rows for item in sublist]
+import argparse
+import sys
+
+if __name__ == "__main__":
+       parser = argparse.ArgumentParser()
+       parser.add_argument("full_tax_file", help="Give the exact name of the input file with all the details")
+       parser.add_argument("pid_file",help="Only one column of search data in the file")
+       args = parser.parse_args()
+       create_pin_file(args.full_tax_file, args.pid_file)
