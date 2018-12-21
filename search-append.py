b6ecac6f700e04cb72941af0543472c22985ad59
Sudheer Bobba <bobbas1@MacBookPro-Bobba.local>
2018-12-21 11:55:44 -0500

    latest

--- search-append.py
+++ search-append.py
@@ -3,12 +3,12 @@ import csv
 
 #def create_pin_file(in_pin_csv, in_all_csv)
 def create_pin_file(full_data_file,pid_file):
-    with open(pid_file,mode='r') as f:
+    with open(pid_file,mode='r',encoding="utf-8") as f:
         f_r = csv.reader(f, delimiter=',')
         rows = [row for row in f_r]
         flat_list = [item for sublist in rows for item in sublist]
 
-    with open(full_data_file,mode='r') as f:
+    with open(full_data_file,mode='r',encoding="utf-8") as f:
         out_file = full_data_file.split('.')[0]+'-out.csv'
         with open(out_file, mode = 'w') as zf:
             f_r = csv.reader(f, delimiter=',')
@@ -16,18 +16,24 @@ def create_pin_file(full_data_file,pid_file):
             header = next(f_r)
             zf_w.writerow(header)
             
+            found_list = []
             for row in f_r:
                 #print row
+                wrote = 0
                 for item in flat_list:
-			#print item
+                        #print item
                     if item in row:
-				#print 'Found matching ' + item
+                                #print 'Found matching ' + item
+                        #wrote = 1
                         zf_w.writerow(row)
-                #print '***************'
-
+                        found_list.append(item)
+                        #break   
+                #if wrote == 0:
+            not_found = [i for i in flat_list if i not in found_list]
+            print(not_found)    
+            for item in not_found:
+                zf_w.writerow([item])
         
-
- 
 import argparse
 import sys
 
