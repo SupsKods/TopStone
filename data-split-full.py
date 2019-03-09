875dca0fb6b21b327db2cb38bf8ddc066f6b22bb
Supriya Koduri <supriya.koduri@turner.com>
2019-03-09 14:56:55 -0500

    test3

--- data-split-full.py
+++ data-split-full.py
@@ -1,33 +1,38 @@
 import csv
 import re
 import os
-#
-#
-#
-#
-#
-#
-os.chdir('C:\\Users\\supbobba\\Desktop\\Residential2018')
 
-f_name = 'OWNDat.dat'
-out_name = 'OWN-Out.dat'
-with open(f_name, 'r') as f: 
-     lines_all = f.readlines() 
-     length = len(lines_all) 
-     print(length) 
-     i =0 
-     inc =5 
-     with open(out_name, 'w') as t: 
-         tw = csv.writer(t) 
-         while True: 
-             row = [] 
-             lines = lines_all[i:i+inc] 
-             for line in lines: 
-                 r = line.strip() 
-                 row.extend(re.split(r'\s{2,}',r)) 
+
+def process_dat_file(f_name):
+    out_name = f_name.split('.')[0]+'-out.dat'
+    #out_name = 'OWN-Out.dat'
+    with open(f_name, 'r') as f: 
+        lines_all = f.readlines() 
+        length = len(lines_all) 
+        print(length) 
+        i =0 
+        inc =5 
+        with open(out_name, 'w') as t: 
+            tw = csv.writer(t) 
+            while True: 
+                row = [] 
+                lines = lines_all[i:i+inc] 
+                for line in lines: 
+                    r = line.strip() 
+                    row.extend(re.split(r'\s{2,}',r)) 
               
-             tw.writerow(row)  
-             i = i+inc 
-             if i > length: 
-                 break 
-             row = []
+                tw.writerow(row)  
+                i = i+inc 
+                if i > length: 
+                    break 
+                row = []
+
+import argparse
+import sys
+ 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("big_dat_file", help="Give the DAT file as input. This will combine 5 lines into one single line")
+    args = parser.parse_args()
+    process_dat_file(args.big_dat_file)
+
