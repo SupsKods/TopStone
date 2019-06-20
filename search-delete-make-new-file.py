ee701734eb4de4fc7c845d7cd9dadcf4c7a5f2dc
Supriya Koduri <supriya.koduri@turner.com>
2019-06-20 11:08:53 -0700

    new file

---
+++ search-delete-make-new-file.py
@@ -0,0 +1,56 @@
+import csv
+import re
+
+
+def search_delete(f_name, in_words_file):
+    with open(in_words_file, 'r',encoding="utf-8-sig") as in_f:
+        #lines_all = in_f.readlines()
+        #print(lines_all)
+        i_f = csv.reader(in_f)
+        rows = list(i_f)
+        list_of_words = [item for sublist in rows for item in sublist]
+        print(list_of_words)
+
+    out_name = f_name.split('.')[0]+'-out.csv'
+    del_name = f_name.split('.')[0]+'-deleted-out.csv'
+    with open(f_name, encoding="utf-8-sig") as f:
+         lines_all = list(csv.reader(f))
+         length = len(lines_all)
+         print(length)
+
+         with open(del_name, 'w', encoding="utf-8") as d, open(out_name, 'w', encoding="utf-8") as t:
+             dw = csv.writer(d)
+             tw = csv.writer(t)
+             skip =0
+             for line in lines_all:
+                 #print(line)
+                 l = []
+                 for item in list_of_words:
+                     # l = [s for s in line if item.lower() in s.lower()]
+                     for s in line:
+                         if item.lower() in s.lower():
+                             l.append(s)
+
+                 if l:
+                     skip = skip+1
+                     dw.writerow(line)
+                    #print(line)
+                 else:
+                     tw.writerow(line)
+    print("Total skipped lines - %d ",skip)
+
+#search_delete('test-pins_2.csv', ['INC','LLC', 'LTD'])
+#search_delete('full-data.csv', 'in-words.txt')
+#search_delete('full-orig-data.csv', 'llc.csv')
+#search_delete('full-data-2.csv', 'Gwin_keys_10.csv')
+#search_delete('Tax_Cobb_2017.csv', 'llc.csv')
+
+import argparse
+import sys
+
+if __name__ == "__main__":
+       parser = argparse.ArgumentParser()
+       parser.add_argument("full_tax_file", help="Give the exact name of the input file with all the details")
+       parser.add_argument("words_file",help="Only one column of search data(case insensitive) in the file")
+       args = parser.parse_args()
+       search_delete(args.full_tax_file, args.words_file)
